fbd3a6630cb5623983ee79a665dd8a42cf29a5ae
Justin Kaufman <jmkauf@gmail.com>
2023-05-15 09:59:59 -0700

    MY_PROJECTS.swift

---
+++ generated/ios/MY_PROJECTS.swift
@@ -0,0 +1,12 @@
+/**
+  This file is auto-generated, do not edit directly
+*/
+import SwiftUI
+
+struct MyProjects: View { 
+    var body: some View {
+        VStack {
+            // ...   
+        }
+    }
+}
